5e20557f4af49c04876d4070db65da1f485755fb
Jason Belcher <jason.belcher@cumberlandgroupit.com>
2019-07-29 23:50:26 -0400

    view comments

--- lessons/data-store/view.js
+++ lessons/data-store/view.js
@@ -5,6 +5,11 @@
 // email
 // gender
 
+/***************************************
+ *
+ * SIMPLE VIEW TO RENDER DATASTORE MODEL
+ */
+
 const container = document.querySelector("#container");
 const output = document.querySelector("#output");
 const form = document.forms[0];
@@ -14,12 +19,17 @@ const interfaceProps = ["first_name", "last_name", "email", "gender", "age"];
 const db = dataStore([], interfaceProps);
 loadPeople();
 
+// initializes localStorage data if exists
+
 function loadPeople() {
   console.log(db.load("people"));
   if (db.load("people") !== null) {
     db.load("people");
     output.innerHTML = printToView();
   }
+
+  // when user clicks on item the datastore record is deleted and then
+  // & removed from the UI view.
 
   output.addEventListener("click", function(e) {
     [].forEach.call(e.path, prop => {
@@ -32,6 +42,8 @@ function loadPeople() {
     db.save("people");
   });
 }
+
+// adds new record to datastore and adds new record to view
 
 addbtn.addEventListener("click", function(e) {
   e.preventDefault();
@@ -57,6 +69,8 @@ addbtn.addEventListener("click", function(e) {
 savebtn.addEventListener("click", function(e) {
   db.save("people");
 });
+
+// append html to view
 
 function printToView() {
   let mappedHTML = "";

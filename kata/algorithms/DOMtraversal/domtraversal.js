34683084aa7f6915b4128bb5cac23b2ace25e9ee
Jason Belcher <jason.belcher@cumberlandgroupit.com>
2019-08-10 13:33:59 -0400

    updates to dom traversal lesson

--- kata/algorithms/DOMtraversal/domtraversal.js
+++ kata/algorithms/DOMtraversal/domtraversal.js
@@ -1,39 +1,53 @@
 // Walk the DOM and find number of occurences of a word or phrase mentioned on a web page.
 
-function walk(node, callback) {
-  if (callback(node) === false) return false;
-  node = node.firstChild;
-  while (node != null) {
-    if (walk(node, callback) === false) return false;
-    node = node.nextSibling;
-  }
-}
-
-let walkedDOM = [];
-
-walk(document.body, node => {
-  if (node.nodeType === 3) {
-    walkedDOM.push(node.nodeValue);
-  }
-});
-
-function search(arr, wordToMatch) {
+function search(wordToMatch) {
+  // drop this function in the chrome console and watch the debugger run.
+  let walkedDOM = [];
   const results = [];
-  const wordRegex = wordToMatch ? new RegExp(wordToMatch, "ig") : false;
-  if (wordRegex) {
-    arr.forEach(textFragment => {
-      textFragment = textFragment.toLowerCase();
-      if (textFragment.match(wordRegex)) {
-        results.push(wordRegex.exec(textFragment));
-      }
-    });
+  function walk(node, callback) {
+    callback(node);
 
-    return { [wordToMatch]: results.length };
-  } else {
-    return console.error(
-      "Please supply some text to the second parameter to search"
-    );
+    node = node.firstChild;
+    // base case
+    while (node != null) {
+      // if base case is not reached then we recurse
+      walk(node, callback);
+
+      node = node.nextSibling;
+    }
+    return node;
   }
+
+  walk(document.body, node => {
+    // set a break point here to see how it recurses and pay special attention to the call stack
+    if (node.nodeType === 3) {
+      walkedDOM.push(node.nodeValue);
+    }
+  });
+
+  // RECURSION DONE
+  const wordRegex = wordToMatch ? new RegExp(wordToMatch, "ig") : false;
+  /*
+   we simply iterate over the array,
+  lowercase each fragment, by this time
+   all recursion is done do a simple regex
+   match against it and if a fragment returns
+   true we push to a results array
+  */
+
+  walkedDOM.forEach(textFragment => {
+    textFragment = textFragment.toLowerCase();
+    if (textFragment.match(wordRegex)) {
+      results.push(wordRegex.exec(textFragment));
+    }
+  });
+
+  /* return number of matches we got on the webpage
+   here we are using a computed key name based on
+  the original word we passed in at the beginning.
+  */
+
+  return { [wordToMatch]: results.length };
 }
 
-console.log(search(walkedDOM, "1"));
+console.log(search("Spiderman"));
